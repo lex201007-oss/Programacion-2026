73266db07f60bb3e2185875850d2b97c97c24e49
Abraham Mogica <lex201007@ciencias.unam.mx>
2026-03-23 23:34:52 -0600

    Update Cliente_inversion.py

--- proyecto_inversiones/Cliente_inversion.py
+++ proyecto_inversiones/Cliente_inversion.py
@@ -3,7 +3,7 @@ class Cliente_inversion:
     # Constructor. Esta clase contiene tres atributos: nombre del cliente y cuenta de inversión
     def __init__(self, nombre, empresa):
         self.nombre = nombre
-        self.empresa= Nombre de la empresa 
+        self.empresa= nombre de la empresa 
 
     def __str__(self):
         cadena = "\nNombre del cliente: " + self.nombre
