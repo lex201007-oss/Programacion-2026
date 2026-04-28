8e5999666a3301d418bc24bcdb0b7f6ba293a4e7
Abraham Mogica <lex201007@ciencias.unam.mx>
2026-04-28 17:35:01 -0600

    Create Main.py

---
+++ Teoria/Objetos_como_cadena/Main.py
@@ -0,0 +1,9 @@
+from Cuenta import *
+from Cliente import *
+class Main:
+    pass
+Cuenta1 = Cuenta("Crédito",2000,"02/03/2020")
+Cliente1 = Cliente("Alexis", "Ciudad de México","02/03/2000",Cuenta1)
+print(Cuenta1)
+print(".........") 
+print(Cliente1)
