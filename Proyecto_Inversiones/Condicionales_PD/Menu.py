5db7cf878d18249dd4b2d8c82ab8cb8fbe976435
Abraham Mogica <lex201007@ciencias.unam.mx>
2026-06-01 18:25:42 -0600

    Update Menu.py

--- Proyecto_Inversiones/Condicionales_PD/Menu.py
+++ Proyecto_Inversiones/Condicionales_PD/Menu.py
@@ -1,7 +1,5 @@
 class Menu:
 
-    def mostrar_menu(self):
-        print("=== SISTEMA DE INVERSIONES ===")
-        print("1. Registrar cliente")
-        print("2. Consultar cuenta")
-        print("3. Salir")
+    def mostrar(self):
+        print("1. Ver clasificación")
+        print("2. Salir")
