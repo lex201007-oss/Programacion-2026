bda9c38ddf9ba0dbc311ef8cdc34d205e1530e78
Abraham Mogica <lex201007@ciencias.unam.mx>
2026-06-06 17:37:10 -0600

    Update Cuenta_Ahorro.py

--- Proyecto_Inversiones/Cuenta_Ahorro.py
+++ Proyecto_Inversiones/Cuenta_Ahorro.py
@@ -3,7 +3,9 @@ from Cuenta import Cuenta
 class Cuenta_Ahorro(Cuenta):
 
     def __init__(self, id_cuenta, saldo, interes):
+
         super().__init__(id_cuenta, saldo)
+
         self.__interes = interes
 
     def obtener_interes(self):
