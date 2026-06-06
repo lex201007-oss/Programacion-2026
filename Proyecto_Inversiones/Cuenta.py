0ae14ae46fbaba199581d799a9570dd32d593984
Abraham Mogica <lex201007@ciencias.unam.mx>
2026-06-06 17:31:50 -0600

    Create Cuenta.py

---
+++ Proyecto_Inversiones/Cuenta.py
@@ -0,0 +1,25 @@
+class Cuenta:
+
+    def __init__(self, id_cuenta, saldo):
+        self.__id_cuenta = id_cuenta
+        self.__saldo = saldo
+
+    def obtener_id(self):
+        return self.__id_cuenta
+
+    def obtener_saldo(self):
+        return self.__saldo
+
+    def depositar(self, cantidad):
+        self.__saldo += cantidad
+
+    def retirar(self, cantidad):
+
+        if cantidad <= self.__saldo:
+            self.__saldo -= cantidad
+            return True
+
+        return False
+
+    def obtener_tipo(self):
+        return "Cuenta"
