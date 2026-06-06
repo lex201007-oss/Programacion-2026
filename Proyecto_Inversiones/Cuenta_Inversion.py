b6ee0066fbf15574a987852dff1524f7095b8c54
Abraham Mogica <lex201007@ciencias.unam.mx>
2026-06-06 17:37:34 -0600

    Update Cuenta_Inversion.py

--- Proyecto_Inversiones/Cuenta_Inversion.py
+++ Proyecto_Inversiones/Cuenta_Inversion.py
@@ -3,7 +3,9 @@ from Cuenta import Cuenta
 class Cuenta_Inversion(Cuenta):
 
     def __init__(self, id_cuenta, saldo, rendimiento):
+
         super().__init__(id_cuenta, saldo)
+
         self.__rendimiento = rendimiento
 
     def obtener_rendimiento(self):
